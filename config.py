805d65431a70b78d8e9c35a6471cfdb998984e9a
max <lkmaksss@gmail.com>
2024-10-17 11:06:21 +0400

    updated for private networking

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname((__file__)))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ewirjiwqeojr322'
-    print(os.environ.get('DATABASE_URL'), os.environ.get('DATABASE_PUBLIC_URL'))
+    print('DB URIs: ', os.environ.get('DATABASE_URL'), os.environ.get('DATABASE_PUBLIC_URL'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('DATABASE_PUBLIC_URL')
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PUBLIC_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
