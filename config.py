ec8ec587edb8089dccc4464608760535d87ad427
max <lkmaksss@gmail.com>
2024-10-17 11:07:17 +0400

    updated for private networking

--- config.py
+++ config.py
@@ -7,7 +7,6 @@ class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'ewirjiwqeojr322'
     print('DB URIs: ', os.environ.get('DATABASE_URL'), os.environ.get('DATABASE_PUBLIC_URL'))
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('DATABASE_PUBLIC_URL')
-    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_PUBLIC_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
 
